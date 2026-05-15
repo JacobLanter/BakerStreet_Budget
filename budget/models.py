9157314045288a54560d12cc313134ee54b22d75
Jacob Lanter <jacobalanter116@gmail.com>
2026-05-15 15:11:03 +0000

    Changed format of project to use Django and PostgreSQL
    Signed-off-by: Jacob Lanter <jacobalanter116@gmail.com>

---
+++ budget/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Category(models.Model):
+    ynab_id = models.UUIDField(unique=True)
+
+    category_group_id = models.UUIDField()
+    category_group_name = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=100)
+
+    hidden = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+
+    budgeted = models.BigIntegerField()
+    activity = models.BigIntegerField()
+    balance = models.BigIntegerField()
+
+    goal_type = models.CharField(max_length=20, blank=True, null=True)
+    goal_target = models.BigIntegerField(blank=True, null=True)
+
+    raw_data = models.JSONField()
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.category_group_name}: {self.name}"
