c5615b6c57c47df803e9223393a78d82f6ed6688
Jacob Lanter <jacobalanter116@gmail.com>
2026-05-15 22:13:13 +0000

    add dashboard URL, view, and template for categories
    Signed-off-by: Jacob Lanter <jacobalanter116@gmail.com>

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Category
 
-# Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+
+    return render(request, "budget/category_list.html", {"categories" : categories},)
