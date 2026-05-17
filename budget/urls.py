ea2655bf8249f34206376c84c76d78a320c740f4
Jacob Lanter <jacobalanter116@gmail.com>
2026-05-17 18:22:22 +0000

    made home page link to dashboard. Future homepage might be added later
    Signed-off-by: Jacob Lanter <jacobalanter116@gmail.com>

--- budget/urls.py
+++ budget/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.dashboard, name="home"),
     path('category_list/', views.category_list, name="category_list"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path("sync-ynab/", views.sync_ynab_now, name="sync_ynab_now"),
